c73e639eb2e704756c7ced2ef57d7c116d9349d7
Zom <contact.zomzd@gmail.com>
2022-07-08 16:56:23 +0200

    #3.1

--- src/main/java/fr/zom/testmod/init/ModFeatures.java
+++ src/main/java/fr/zom/testmod/init/ModFeatures.java
@@ -20,12 +20,13 @@ public class ModFeatures {
     public static final DeferredRegister<ConfiguredFeature<?, ?>> FEATURES = DeferredRegister.create(Registry.CONFIGURED_FEATURE_REGISTRY, TestMod.MODID);
 
     public static final RegistryObject<ConfiguredFeature<OreConfiguration, Feature<OreConfiguration>>> ORE_RED_EMERALD_FEATURE = FEATURES.register("ore_red_emerald_feature", () -> new ConfiguredFeature<>(Feature.ORE,
-            new OreConfiguration(new TagMatchTest(BlockTags.STONE_ORE_REPLACEABLES), ModBlocks.RED_EMERALD_ORE.get().defaultBlockState(), 3)));
+            new OreConfiguration(List.of(OreConfiguration.target(new TagMatchTest(BlockTags.STONE_ORE_REPLACEABLES), ModBlocks.RED_EMERALD_ORE.get().defaultBlockState())), 7)));
 
     public static final DeferredRegister<PlacedFeature> PLACED_FEATURES = DeferredRegister.create(Registry.PLACED_FEATURE_REGISTRY, TestMod.MODID);
 
     public static final RegistryObject<PlacedFeature> ORE_RED_EMERALD = PLACED_FEATURES.register("ore_red_emerald",
             () -> new PlacedFeature(Holder.direct(ORE_RED_EMERALD_FEATURE.get()),
-                    List.of(InSquarePlacement.spread(), BiomeFilter.biome(), CountPlacement.of(35),
-                            HeightRangePlacement.uniform(VerticalAnchor.absolute(-16), VerticalAnchor.absolute(32)))));
+                    List.of(InSquarePlacement.spread(), BiomeFilter.biome(), CountPlacement.of(55),
+                            HeightRangePlacement.uniform(VerticalAnchor.absolute(8), VerticalAnchor.absolute(50)))));
+
 }
